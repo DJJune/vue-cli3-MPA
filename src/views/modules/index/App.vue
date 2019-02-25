345b07b7ba9a7d7a01bbbd73286963152a4787c7
WRF <wrfefeng>
2019-02-25 10:32:55 +0800

    2019-02-25 修改主页样式

--- src/views/modules/index/App.vue
+++ src/views/modules/index/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <a href="console.html">console</a>
-      <a href="client.html">client</a>
+      <h1>主页面</h1>
+      <a href="console.html">日志页</a>
+      <a href="client.html">客户端</a>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+  #app {
+    a {
+      &:last-child {
+        display: inline-block;
+        margin-left: 30px;
+      }
+    }
+  }
 </style>
