c40281f24420932be5e06d0251a7bfb95d8d468d
WRF <wrfefeng>
2019-02-25 10:46:14 +0800

    2019-02-25 js实现跳转

--- src/views/modules/index/App.vue
+++ src/views/modules/index/App.vue
@@ -2,19 +2,50 @@
   <div id="app">
     <div id="nav">
       <h1>主页面</h1>
-      <a href="console.html">日志页</a>
-      <a href="client.html">客户端</a>
+      <div>
+        <div class="client" @click="route(0)">客户端</div>
+        <div class="console" @click="route(1)">日志页</div>
+      </div>
+      <div>
+        <a class="client" href="client.html">客户端</a>
+        <a class="console" href="console.html">日志页</a>
+      </div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {},
+  computed: {},
+  methods: {
+    route (category) {
+      console.log(category)
+      if (category) {
+        // 跳转至日志页
+        window.location.href = 'console.html'
+      } else {
+        // 跳转至客户端页面
+        window.location.href = 'client.html'
+      }
+    }
+  }
+}
+</script>
+
 <style lang="scss">
   #app {
-    a {
-      &:last-child {
-        display: inline-block;
-        margin-left: 30px;
-      }
+    .client, .console {
+      display: inline-block;
+      border: 1px solid #ccc;
+      padding: 3px 15px;
+      margin: 10px 20px;
+      font-size: 14px;
+      color: blue;
+      cursor: pointer
     }
   }
 </style>
